69db04c98f1087108abe38ac2a266fbc58376014
Saha,Sounak <Sounak.Saha@misys.com>
2020-02-03 22:04:21 +0530

    updated

---
+++ ArchiveLibrary/__init__.py
@@ -0,0 +1,11 @@
+from .keywords import ArchiveKeywords
+from .version import VERSION
+
+_version_ = VERSION
+
+
+class ArchiveLibrary(ArchiveKeywords):
+    ''' ArchiveLibrary is a robot framework keyword library to
+    handle ZIP and possibly other archive formats.
+    '''
+    ROBOT_LIBRARY_SCOPE = 'GLOBAL'
